335bf6c4312b8078016bbb8c657b87b14eef045f
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2024-01-10 14:52:12 +0800

    changed temperature and added explain function

--- main.py
+++ main.py
@@ -23,13 +23,19 @@ print(openai_api_key)
 
 # initialise ChatModel with API key
 llm = ChatOpenAI(
-    openai_api_key=openai_api_key
+    openai_api_key=openai_api_key,
+    temperature=1
 )
 
 print("initialized successfully")
 
 def add_comments_to_code(input_code, language):
     query = "Add comments to the following " + language + " code while preserving the structure of the original code: " + input_code
+    message = [HumanMessage(content=query)]
+    return llm.invoke(message).content
+
+def explain_code(input_code, language):
+    query = "Explain the following " + language + " code: " + input_code
     message = [HumanMessage(content=query)]
     return llm.invoke(message).content
 
@@ -40,14 +46,19 @@ def main():
     st.write("Documentation: [GitHub](https://github.com/lukeskywalker22/Codementator/tree/main)")
 
     option = st.selectbox('Select programming language',('Python', 'Javascript', 'C', 'Java', 'Swift', 'Typescript', 'Rust', 'Ruby', 'CSS', 'HTML', 'C++'))
+    explaintoggle = st.toggle("Explain mode")
     code_input = st.text_area("Enter your code snippet:", height=200)
+
 
     # Button to trigger code commenting (not functional in this template)
     if st.button("Add Comments"):
         # Perform code commenting (replace with actual logic)
         commented_code = add_comments_to_code(code_input, option)
+        explained_code = explain_code(code_input, option)
 
         # Display the commented code in a separate text widget
         st.text_area("Code with Comments:", value=commented_code, height=200)
+        if explaintoggle:
+            st.text_area("Explained code:", value=explained_code, height=200)
 
 main()
