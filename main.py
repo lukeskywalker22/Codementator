53499988fafe79b043f1f56faec2a759ba9050ea
luke yeo <87899265+lukeskywalker22@users.noreply.github.com>
2024-01-10 14:11:27 +0800

    cleaned up file structure and added streamlit ui

--- main.py
+++ main.py
@@ -2,37 +2,37 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 # load langchain libraries
+from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
+from langchain.schema import HumanMessage
 
 import streamlit as st
 
+basedirectory = os.path.dirname(__file__)
+
 # load OPENAI API key
-load_dotenv('env.txt')
+load_dotenv(os.path.join(os.path.dirname(__file__), "env.txt"))
 openai_api_key = os.getenv('OPENAI_API_KEY')
 print(openai_api_key)
 
 # initialise ChatModel with API key
-chat_model = ChatOpenAI(
-    model_name = 'gpt-3.5-turbo',
-    temperature=0.5,
+llm = ChatOpenAI(
     openai_api_key=openai_api_key
 )
+print("initialized successfully")
 
 def add_comments_to_code(input_code):
-    # Function to add comments to the input code (replace with your logic)
-    # Currently, it simply adds a comment prefix to each line.
-    commented_code = "\n".join(["# " + line for line in input_code.split("\n")])
-    return commented_code
+    query = "Add comments to the following python code: " + input_code
+    message = [HumanMessage(content=query)]
+    return llm.invoke(message).content
 
 # Streamlit app layout
 def main():
     st.title("Code Commenter App")
-
-    # Input textarea for code snippet
     code_input = st.text_area("Enter your code snippet:", height=200)
 
     # Button to trigger code commenting (not functional in this template)
@@ -44,4 +44,3 @@ def main():
         st.text_area("Code with Comments:", value=commented_code, height=200)
 
 main()
-
